6bd6136ae784b40b63da5d904a8dd1afd377af09
atifkhalilkhan <info.atifkhalilkhan@gmail.com>
2026-02-20 06:49:07 +0500

    worked

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -65,4 +65,33 @@ userSchema.pre("save", async function (next) {
 userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password);
 }
+
+userSchema.methods.generateAccessToken = function () {
+   return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname,
+
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN }
+    )
+}
+
+userSchema.methods.generaterefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname,    
+
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN }
+    )   
+    
+}
 export const User = mongoose.model("User", userSchema);
